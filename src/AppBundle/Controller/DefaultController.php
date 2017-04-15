96304b05a56df0cbb6461bf585fb7c6fc97d35aa
lukasztecza <lukasztecza.pl@gmail.com>
2017-04-15 01:19:10 -0400

    localization

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,60 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * This route is hit if no locale is set in url
+     */
+    public function pickLanguageAction(Request $request)
+    {
+        $cookies = $request->cookies;
+        $language = $cookies->get('language') ?? $request->getLocale();
+        return $this->redirectToRoute('app_default_index', ['_locale' => $language]);
+    }
+
+    /**
+     * @Route("/switch-language/{language}", requirements={"language": "[a-z]{2}"})
+     */
+    public function switchLanguageAction(Request $request, string $language)
+    {
+        $refererRoute = $this->container->get('referer_manager')->getRefererRoute();
+        $allowedLocales = explode('|', $this->container->getParameter('app.locales'));
+        if (!in_array($language, $allowedLocales)) {
+            $language = $request->getLocale();
+        }
+
+        $router = $this->container->get('router');
+        if (empty($refererRoute['_route'])) {
+            $response = new RedirectResponse($router->generate('app_default_index', ['_locale' => $language]));
+        } else {
+            $response =  new RedirectResponse($router->generate($refererRoute['_route'], ['_locale' => $language]));
+        }
+
+        $cookie = new Cookie('language', $language, time() + 604800); // One week
+        $response->headers->setCookie($cookie);
+        return $response;
+    }
+
+    /**
+     * @Route("/")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/tester")
+     */
+    public function testerAction(Request $request)
+    {
+        return $this->render('default/tester.html.twig', ['count' => 11] );
+    }
+
 }
